59af9cca26549905c868e1420ccc774a09fe3319
Migarca <migarciacampos@gmail.com>
2024-10-08 18:27:00 +0200

    add swagger

--- src/business/business.controller.ts
+++ src/business/business.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { BusinessService } from './business.service';
 import { CreateBusinessDto } from './dto/create-business.dto';
 import { UpdateBusinessDto } from './dto/update-business.dto';
 
+@ApiTags('business')
 @Controller('business')
 export class BusinessController {
   constructor(private readonly businessService: BusinessService) { }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // Allow only DTOs properties
     forbidNonWhitelisted: true,
     transform: true, // Transform DTOs into instances
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Book Hub')
+    .setDescription('The booking Hub API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
-  Logger.log('Server running on port 3000');
+  Logger.verbose(`Server running on port ${app.getHttpServer().address().port}`);
 }
 bootstrap();
