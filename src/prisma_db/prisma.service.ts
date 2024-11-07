8335d1ece60e450062a48397a4ec4921527d442a
Migarca <migarciacampos@gmail.com>
2024-11-07 20:04:19 +0100

    add prisma verbose log

--- src/prisma_db/prisma.service.ts
+++ src/prisma_db/prisma.service.ts
@@ -1,9 +1,17 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+    private readonly logger = new Logger(PrismaService.name);
     async onModuleInit() {
-        await this.$connect();
+        try {
+            await this.$connect();
+            this.logger.verbose('Successfully connected to the Prisma database.');
+        } catch (error) {
+            this.logger.error('Failed to connect to the Prisma database.', error.stack);
+            throw error;
+        }
+
     }
 }
