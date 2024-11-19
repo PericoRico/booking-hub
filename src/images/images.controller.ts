fd35e2507a061b8765bec42b2cc642569ff1731d
Migarca <migarciacampos@gmail.com>
2024-11-19 21:40:28 +0100

    add swagger to images

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, HttpStatus, ParseFilePipeBuilder, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('images')
 @Controller('images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) { }
