1ea5d76f3bef0b3d834c0463427a9a7db430a553
Migarca <migarciacampos@gmail.com>
2025-02-10 13:19:55 +0100

    add response dto to signIn

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { SignInDto } from './dto/sign-in.dto';
+import { SignInResponseDto } from './dto/sign-in.response.dto';
+import { ApiResponse } from '@nestjs/swagger';
 
 @ApiTags('Authentication')
 @Controller('auth')
@@ -9,7 +11,12 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  signIn(@Body() signInDto: SignInDto) {
+  @ApiResponse({
+    status: 200,
+    description: 'User successfully logged in',
+    type: SignInResponseDto
+  })
+  signIn(@Body() signInDto: SignInDto): Promise<SignInResponseDto> {
     return this.authService.signIn(signInDto.email, signInDto.password);
   }
 }
