9f3a61fe39de0d4073753fb55cdee708b051f1e3
Migarca <migarciacampos@gmail.com>
2025-02-10 13:13:06 +0100

    add signIn dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiTags } from '@nestjs/swagger';
+import { SignInDto } from './dto/sign-in.dto';
 
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  signIn(@Body() signInDto: { email: string; password: string }) {
+  signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto.email, signInDto.password);
   }
 }
---
+++ src/auth/dto/sign-in.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SignInDto {
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'user@example.com'
+  })
+  email: string;
+
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'password123'
+  })
+  password: string;
+}
